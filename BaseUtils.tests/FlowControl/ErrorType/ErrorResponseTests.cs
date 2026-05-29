b94c7651aa5f2895785097a5f376293927e5434f
MateuSerafim <mateus.serafim.dev@gmail.com>
2026-05-28 22:50:41 -0300

    bugfix: add tests

--- BaseUtils.tests/FlowControl/ErrorType/ErrorResponseTests.cs
+++ BaseUtils.tests/FlowControl/ErrorType/ErrorResponseTests.cs
@@ -83,4 +83,46 @@ public class ErrorResponseTests
         Assert.Equal(string.Join(", ", values.GetStringsByGenericCollection()), response.ErrorValue);
         Assert.IsType<Guid>(response.ErrorId);
     }
+
+    [Fact(DisplayName = "ERT-01.01.05: Create Invalid Operation Error. Mensage value in empty list")]
+    public void CreateInvalidOperationError5()
+    {
+        // Given
+        Fixture fixture = new();
+        string code = fixture.Create<string>();
+        string message = fixture.Create<string>() + ErrorResponse.ReferenceToVariable;
+        
+        List<int> values = [];
+
+        // When
+        ErrorResponse response = ErrorResponse.Create(code, message, values);
+
+        // Then
+        Assert.Equal(code, response.ErrorCode);
+        Assert.Equal(message.Replace(ErrorResponse.ReferenceToVariable, 
+        string.Join(", ", values.GetStringsByGenericCollection())), 
+                     response.ErrorMessage());
+        Assert.Equal(string.Join(", ", values.GetStringsByGenericCollection()), response.ErrorValue);
+        Assert.IsType<Guid>(response.ErrorId);
+    }
+
+    [Fact(DisplayName = "ERT-01.01.06: Create Invalid Operation Error. Mensage value in empty list")]
+    public void CreateInvalidOperationError6()
+    {
+        // Given
+        Fixture fixture = new();
+        string code = fixture.Create<string>();
+        string message = fixture.Create<string>() + ErrorResponse.ReferenceToVariable;
+
+
+        // When
+        #pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+        ErrorResponse response = ErrorResponse.Create<List<int>>(code, message, null);
+        #pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+
+        // Then
+        Assert.Equal(code, response.ErrorCode);
+        Assert.Equal(message, response.ErrorMessage());
+        Assert.IsType<Guid>(response.ErrorId);
+    }
 }
